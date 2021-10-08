1f9599cc91ebe1a73c253edf4fbd45b9a7bfad7e
Sutthinart Khunvadhana <iakgoog@gmail.com>
2021-10-08 20:19:22 +0700

    migrate to Chokidar

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
-const cron = require("node-cron");
 const chalk = require("chalk");
+const chokidar = require("chokidar");
+const path = require("path");
 
-cron.schedule("0,5,10,15,20,25,30,35,40,45,50,55 * * * * *", (i) => {
-  console.log(chalk.bgBlue.bold("HELLO WORLD", i));
+const watchDir = path.join(__dirname, "..", "watch");
+
+const watcher = chokidar.watch(watchDir, {
+  ignored: /(^|[\/\\])\../, // ignore dotfiles
+  persistent: true,
 });
+
+const log = console.log.bind(console);
+
+watcher.on("add", (path) => log(chalk.red(`File ${path} has been added`)));
